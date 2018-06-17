7a6b935078f356d2dca9cb8b2c95a31590b3dd3a
Kevin Tjiam <kevin@kevincharm.com>
2018-06-17 16:46:40 +0800

    simplify required manifest keys check

--- src/ManifestAsset.js
+++ src/ManifestAsset.js
@@ -243,10 +243,7 @@ class ManifestAsset extends Asset {
 
     hasWebExtensionManifestKeys() {
         const requiredKeys = ['manifest_version', 'name', 'version']
-        const presentKeys = Object.keys(this.ast).filter(key =>
-            requiredKeys.includes(key)
-        )
-        return presentKeys.length === requiredKeys.length
+        return requiredKeys.every(key => !!this.ast[key])
     }
 
     collectDependencies() {

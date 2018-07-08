370d7a57f0b1198799ee20482c457a9cffcb74fa
David Rogers <al-the-x@users.noreply.github.com>
2018-07-08 00:06:04 -0400

    Handle multiple icons in `processBrowserOrPageAction`

--- src/ManifestAsset.js
+++ src/ManifestAsset.js
@@ -167,9 +167,8 @@ class ManifestAsset extends Asset {
             this.isAstDirty = true
         }
         if (action.default_icon) {
-            action.default_icon = this.processSingleDependency(
-                action.default_icon
-            )
+            action.default_icon = this.processAllIcons(action.default_icon)
+
             this.isAstDirty = true
         }
     }
@@ -189,16 +188,22 @@ class ManifestAsset extends Asset {
         }
     }
 
+    processAllIcons(icons) {
+        for (const size of Object.keys(icons)) {
+            icons[size] = this.processSingleDependency(icons[size])
+        }
+    }
+
     processIcons(nodeName) {
         if (nodeName !== 'icons') {
             return
         }
 
         const icons = this.ast[nodeName]
-        for (const size of Object.keys(icons)) {
-            icons[size] = this.processSingleDependency(icons[size])
-            this.isAstDirty = true
-        }
+
+        this.processAllIcons(icons)
+
+        this.isAstDirty = true
     }
 
     collectDependenciesForWebExtension() {
